2776cff9a2709c8afe81defbb9b5b3307e2b29f9
Real_IceyDev <78712036+ReallyDev@users.noreply.github.com>
2022-07-07 14:27:27 -0400

    Added pause.js
    Pause the current song/track.

---
+++ commands/Music/pause.js
@@ -0,0 +1,54 @@
+const { Command } = require("reconlx");
+const ee = require("../../settings/embed.json");
+const config = require("../../settings/config.json");
+const emoji = require("../../settings/emoji.json");
+const player = require("../../handlers/player");
+const { check_dj  } = require("../../handlers/functions");
+
+module.exports = new Command({
+  name: "pause",
+  description: `Pause the current song/track.`,
+  userPermissions: ["CONNECT"],
+  botPermissions: ["CONNECT"],
+  category: "Music",
+  cooldown: 5,
+  DJ: true,
+
+  run: async ({ client, interaction, args, prefix }) => {
+    let channel = interaction.member.voice.channel;
+    let queue = await player.getQueue(interaction.guild.id);
+    if (!channel) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You Need to Join Voice Channel first **`
+      );
+    } else if (
+      interaction.guild.me.voice.channel &&
+      !interaction.guild.me.voice.channel.equals(channel)
+    ) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You Need to Join __My__ Voice Channel **`
+      );
+    } else if (interaction.guild.me.voice.serverMute) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} I am Muted in Voice Channel , unmute me first **`
+      );
+    } else if (!queue) {
+      return interaction.followUp(`** ${emoji.ERROR} Nothing Playing Now **`);
+    } else if (check_dj(client, interaction.member, queue.songs[0])) {
+      return interaction.followUp(
+        `** ${emoji.ERROR} You are Not DJ and also not Song Requester **`
+      );
+    } else if (queue.paused) {
+      return interaction.followUp(`** ${emoji.ERROR} Song is Already Paused **`);
+    } else {
+      await queue.pause();
+      interaction.followUp(`** ${emoji.pause} Song Paused **`);
+    }
+  },
+});
+
+/**
+Coded by: Real_IceyDev
+License: Creative Commons Legal Code
+Whetū, best music bot of 2022.
+*/
