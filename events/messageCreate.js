071822c4b5776cf9094c5ca6acbc89915d7cbbfb
Real_IceyDev <78712036+ReallyDev@users.noreply.github.com>
2022-07-07 14:57:07 -0400

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+const client = require("..");
+const ee = require("../settings/embed.json");
+const { databasing } = require('../handlers/functions')
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot || !message.guild) return;
+  databasing(message.guild.id,message.member.id)
+  let prefix = "!"
+  let mentionprefix = new RegExp(
+    `^(<@!?${client.user?.id}>|${mentionprefixnew(prefix)})`
+  );
+  if (!mentionprefix.test(message.content)) return;
+  const [, nprefix] = message.content.match(mentionprefix);
+  if (nprefix.includes(client.user.id)) {
+    message.reply(`**To see all of my commands, type: \`/help\`**`);
+  }
+  let args = message.content.slice(nprefix.length).trim().split(/ +/)
+  let cmd = args.shift()?.toLowerCase()
+  if(cmd === "rr"){
+    process.exit()
+    message.reply(`RESTARTING............`)
+  }
+});
+
+function mentionprefixnew(newprefix) {
+  return newprefix.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
+}
+
+/**
+Coded by: Real_IceyDev
+License: Creative Commons Legal Code
+Whetū, best music bot of 2022.
+*/
